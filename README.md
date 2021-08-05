8819eeb8617cdb1a2abce4bb52eaf1afeae61727
Carlos Jorge <cacjorge@gmail.com>
2021-08-05 15:19:23 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DesafioCerti
+# Desafio Certi
+
+Instruções de uso
+
+1 - Tenha o NodeJS instalado na sua máquina, que pode ser baixado pelo site https://nodejs.org/
+
+2 - Baixe o arquivos index.js deste repositório
+
+3 - Abra o terminal e vá até a pasta onde se encontram o arquivo baixado do repositório
+
+4 - Instale a biblioteca "written-number" utilizando o comando:
+
+```
+npm install --save written-number
+```
+
+4 - Digite "node index.js". O programa começará a rodar
+
+5 - Crie uma requisição HTTP do tipo GET para o localhost, na porta 3000, com um parâmetro sendo o número a ser transformado em extenso. É possível fazer isso digitando "http://localhost:3000/XXXX" no seu browser, onde XXXX é o parâmetro.
+
+6 - O programa irá retornar um JSON com uma chave "extenso" e seu valor como sendo o número de entrada escrito por extenso.
---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+* @author     Carlos Antonio
+* @version    0.1
+*/
+/*
+servidor HTTP que recebe uma requisicao GET e retorna um JSON
+com uma chave "numExtenso", que eh a versao por numExttenso de um 
+numero inteiro enviado no path.
+*/
+
+var writtenNumber = require('written-number');
+
+const http = require('http')
+const port = 3000
+const ip = 'localhost'
+
+const server = http.createServer(function (req, res) {
+	var valor=req.url.slice(1);
+	var extenso='';
+	
+	if(valor>=-999999 && valor < 0){
+		extenso="menos"+" ";
+		extenso+=writtenNumber(valor*(-1), {lang: 'pt'});
+		res.writeHead(200, {"context-type":"application/json"});
+	    res.write(JSON.stringify({ "extenso": extenso }));
+	    res.end();
+	}
+	else if (valor>=0 && valor<=999999){
+			extenso+=writtenNumber(valor, {lang: 'pt'});
+			res.writeHead(200, {"context-type":"application/json"});
+			res.write(JSON.stringify({ "extenso": extenso }));
+			res.end();
+		}
+    else{
+		res.writeHead(200, { "Content-Type": "text/plain" });
+		res.write(JSON.stringify( "o numero dever entre -999999 e 999999" ));
+		res.end();
+        
+    }
+})
+
+server.listen(port, ip, () => {
+  console.log(`Servidor rodando em http://${ip}:${port}`);
+})
