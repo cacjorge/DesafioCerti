e1d90103a41beb895449f7f7fb14be220b18dd45
Carlos Jorge <cacjorge@gmail.com>
2021-08-06 15:36:18 -0300

    removendo biblioteca de terceiros
    removendo a biblioteca written-number e inserindo uma função própria para identificar o número por extenso.

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
 /**
 * @author     Carlos Antonio
-* @version    0.1
+* @version    0.2
 */
 /*
 servidor HTTP que recebe uma requisicao GET e retorna um JSON
-com uma chave "extenso", que eh a versao por extenso de um 
+com uma chave "numExtenso", que eh a versao por numExttenso de um 
 numero inteiro enviado no path.
 */
 
-var writtenNumber = require('written-number');
+const { numExtenso } = require('./numExtenso');
 
-const http = require('http')
-const port = 3000
-const ip = 'localhost'
+const http = require('http');
+const port = 3000;
+const ip = 'localhost';
 
-const server = http.createServer(function (req, res) {
-	var valor=req.url.slice(1);
-	var extenso='';
-	
-	if(valor>=-999999 && valor < 0){
-		extenso="menos"+" ";
-		extenso+=writtenNumber(valor*(-1), {lang: 'pt'});
-		res.writeHead(200, {"context-type":"application/json"});
-	    res.write(JSON.stringify({ "extenso": extenso }));
-	    res.end();
-	}
-	else if (valor>=0 && valor<=999999){
-			extenso+=writtenNumber(valor, {lang: 'pt'});
-			res.writeHead(200, {"context-type":"application/json"});
-			res.write(JSON.stringify({ "extenso": extenso }));
-			res.end();
-		}
-    else{
-		res.writeHead(200, { "Content-Type": "text/plain" });
-		res.write(JSON.stringify( "o numero dever entre -999999 e 999999" ));
-		res.end();
-        
+const server = http.createServer((req, res) => {
+  	numExt = numExtenso(req.url.substring(1));
+    if(numExt == 1){
+        res.end("path com formato errado");
     }
+    else if (numExt == 2) {
+        res.end("o numero dever estar entre -99999 e 99999");
+    }
+    else{
+        res.end('{ \"extenso\": \"' + numExt + '\" }');
+    }
+
 })
 
 server.listen(port, ip, () => {
